23d7ca25179585baf6e9ccb5b4259a8dec0c9aad
Amay <brajbliss@gmail.com>
2021-10-17 16:42:56 +0530

    middlewares and first request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 dotenv.config();
 
@@ -9,6 +11,15 @@ mongoose
 	.connect(process.env.MONGO_URL)
 	.then(() => console.log('DB connection successful'))
 	.catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+	res.send('Welcome to homepage');
+});
 
 app.listen(8800, () => {
 	console.log('BackEnd server is running');
