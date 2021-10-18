1724e3e0535540c2f9ac9b3485101db562888ae7
Amay <brajbliss@gmail.com>
2021-10-18 19:17:50 +0530

    react router

--- client/src/components/closeFriend/CloseFriend.jsx
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -1,10 +1,16 @@
-import "./closeFriend.css";
+import './closeFriend.css';
 
-export default function CloseFriend({user}) {
-  return (
-    <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
-      <span className="sidebarFriendName">{user.username}</span>
-    </li>
-  );
+export default function CloseFriend({ user }) {
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+	return (
+		<li className='sidebarFriend'>
+			<img
+				className='sidebarFriendImg'
+				src={PF + user.profilePicture}
+				alt=''
+			/>
+			<span className='sidebarFriendName'>{user.username}</span>
+		</li>
+	);
 }
