cbab259e0c4beddd20d8ad0e333613d434dac788
Amay <brajbliss@gmail.com>
2021-10-22 17:46:45 +0530

    fetching user friends

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,27 @@
 import './rightbar.css';
 import { Users } from '../../dummyData';
 import Online from '../online/Online';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function Rightbar({ user }) {
 	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+	const [friends, setFriends] = useState([]);
+
+	useEffect(() => {
+		const getFriends = async () => {
+			try {
+				const friendList = await axios.get(
+					'/users/friends/' + user._id
+				);
+				setFriends(friendList.data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getFriends();
+	}, [user._id]);
 
 	const HomeRightbar = () => {
 		return (
@@ -52,16 +70,26 @@ export default function Rightbar({ user }) {
 				</div>
 				<h4 className='rightbarTitle'>User friends</h4>
 				<div className='rightbarFollowings'>
-					<div className='rightbarFollowing'>
-						<img
-							src={`${PF}person/1.jpeg`}
-							alt=''
-							className='rightbarFollowingImg'
-						/>
-						<span className='rightbarFollowingName'>
-							John Carter
-						</span>
-					</div>
+					{friends.map((friend) => (
+						<Link
+							to={'/profile/' + friend.username}
+							style={{ textDecoration: 'none' }}>
+							<div className='rightbarFollowing'>
+								<img
+									src={
+										friend.profilePicture
+											? PF + friend.profilePicture
+											: PF + 'person/noAvatar.png'
+									}
+									alt=''
+									className='rightbarFollowingImg'
+								/>
+								<span className='rightbarFollowingName'>
+									{friend.username}
+								</span>
+							</div>
+						</Link>
+					))}
 				</div>
 			</>
 		);
