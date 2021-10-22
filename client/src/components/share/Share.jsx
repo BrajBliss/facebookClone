1a8645772c70a80668309fffb01d577735ee9ac4
Amay <brajbliss@gmail.com>
2021-10-22 16:05:06 +0530

    text and image post axios

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,40 +1,89 @@
-import "./share.css";
-import {PermMedia, Label,Room, EmojiEmotions} from "@material-ui/icons"
+import './share.css';
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons';
+import { useContext, useRef, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 export default function Share() {
-  return (
-    <div className="share">
-      <div className="shareWrapper">
-        <div className="shareTop">
-          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
-          <input
-            placeholder="What's in your mind Safak?"
-            className="shareInput"
-          />
-        </div>
-        <hr className="shareHr"/>
-        <div className="shareBottom">
-            <div className="shareOptions">
-                <div className="shareOption">
-                    <PermMedia htmlColor="tomato" className="shareIcon"/>
-                    <span className="shareOptionText">Photo or Video</span>
-                </div>
-                <div className="shareOption">
-                    <Label htmlColor="blue" className="shareIcon"/>
-                    <span className="shareOptionText">Tag</span>
-                </div>
-                <div className="shareOption">
-                    <Room htmlColor="green" className="shareIcon"/>
-                    <span className="shareOptionText">Location</span>
-                </div>
-                <div className="shareOption">
-                    <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
-                    <span className="shareOptionText">Feelings</span>
-                </div>
-            </div>
-            <button className="shareButton">Share</button>
-        </div>
-      </div>
-    </div>
-  );
+	const { user } = useContext(AuthContext);
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+	const desc = useRef();
+
+	const [file, setFile] = useState(null);
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		const newPost = {
+			userId: user._id,
+			desc: desc.current.value,
+		};
+
+		try {
+			await axios.post('/posts', newPost);
+		} catch (err) {}
+	};
+
+	return (
+		<div className='share'>
+			<div className='shareWrapper'>
+				<div className='shareTop'>
+					<img
+						className='shareProfileImg'
+						src={
+							user.profilePicture
+								? PF + user.profilePicture
+								: PF + 'person/noAvatar.png'
+						}
+						alt=''
+					/>
+					<input
+						placeholder={
+							"What's in your mind " + user.username + '?'
+						}
+						className='shareInput'
+						ref={desc}
+					/>
+				</div>
+				<hr className='shareHr' />
+				<form className='shareBottom' onSubmit={submitHandler}>
+					<label htmlFor='file' className='shareOptions'>
+						<div className='shareOption'>
+							<PermMedia
+								htmlColor='tomato'
+								className='shareIcon'
+							/>
+							<span className='shareOptionText'>
+								Photo or Video
+							</span>
+							<input
+								style={{ display: 'none' }}
+								type='file'
+								id='file'
+								accept='.png,.jpeg,.jpg'
+								onChange={(e) => setFile(e.target.files[0])}
+							/>
+						</div>
+						<div className='shareOption'>
+							<Label htmlColor='blue' className='shareIcon' />
+							<span className='shareOptionText'>Tag</span>
+						</div>
+						<div className='shareOption'>
+							<Room htmlColor='green' className='shareIcon' />
+							<span className='shareOptionText'>Location</span>
+						</div>
+						<div className='shareOption'>
+							<EmojiEmotions
+								htmlColor='goldenrod'
+								className='shareIcon'
+							/>
+							<span className='shareOptionText'>Feelings</span>
+						</div>
+					</label>
+					<button className='shareButton' type='submit'>
+						Share
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
